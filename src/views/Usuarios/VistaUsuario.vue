107a3fb88c2dc762005faa1c2fd0c6e863c5329a
Eduard <eduard@localhost.localdomain>
2022-07-24 04:28:06 -0400

    cambios weeb

--- src/views/Usuarios/VistaUsuario.vue
+++ src/views/Usuarios/VistaUsuario.vue
@@ -12,10 +12,10 @@
                     <h1 class="TextoBold" v-if="Usuario.PoliciaR!=null">{{Usuario.PoliciaR.Nombre+" "+Usuario.PoliciaR.Paterno+" "+Usuario.PoliciaR.Materno}}</h1>
                 </div>
                 <div v-if="Usuario!=null" style="position:absolute;right:10px;top:200px;z-index:100;">
-                    <h1 class="TextoBold">Editar : <a-switch :checked="false" @change="onChange" /></h1>
+                    <h1 class="TextoBold">Editar : <a-switch :checked="Editando" @change="EditarStatusSet" /></h1>
                 </div>
             </a-col>
-            <a-col :span="24">
+            <a-col v-if="Editando==false" :span="24">
                 <a-col :span="18" style="padding:5px;">
                     <a-card v-if="Usuario!=null" :bordered="true" style="width: 100%;">
                         <h6 class="TextoBold" v-if="Usuario.Jerarquia!=null">Grado : {{Usuario.Jerarquia.Grado}}</h6>
@@ -39,52 +39,54 @@
                     </a-card>
                 </a-col>
             </a-col>
-            <a-col :span="6" :style="{padding:'10px'}">
-                <a-card title="Grado" :bordered="false" style="width: 100%;">
-                    <a-switch default-checked @change="onChange" />
-                </a-card>
-                <a-card title="Rango" :bordered="false" style="width: 100%;">
-                    <a-switch default-checked @change="onChange" />
-                </a-card>
-            </a-col>
-            <a-col :span="18">
-                <a-row>
-                    <a-col :span="8">
-                        <b-form-group class="col-md-11" label="Escalafon" label-for="Escalafon" label-cols-sm="12" label-align-sm="right" >
-                            <b-form-input id="Escalafon"></b-form-input>
-                        </b-form-group>
-                    </a-col>
-                    <a-col :span="8">
-                        <b-form-group class="col-md-11" label="Nombre" label-for="Nombre" label-cols-sm="12" label-align-sm="right" >
-                            <b-form-input id="Nombre"></b-form-input>
-                        </b-form-group>
-                    </a-col>
-                    <a-col :span="8">
-                        <b-form-group class="col-md-11" label="Apellido Materno" label-for="Apellido Materno" label-cols-sm="12" label-align-sm="right" >
-                            <b-form-input id="Materno"></b-form-input>
-                        </b-form-group>
-                    </a-col>
-                    <a-col :span="8">
-                        <b-form-group class="col-md-11" label="Apellido Paterno" label-for="Apellido Paterno" label-cols-sm="12" label-align-sm="right" >
-                            <b-form-input id="Paterno"></b-form-input>
-                        </b-form-group>
-                    </a-col>
-                    <a-col :span="8">
-                        <b-form-group class="col-md-11" label="Telefono" label-for="Telefono" label-cols-sm="12" label-align-sm="right" >
-                            <b-form-input id="Telefono"></b-form-input>
-                        </b-form-group>
-                    </a-col>
-                </a-row>
-                <a-row>
-                    <a-col :span="24" :style="{marginTop:'20px'}">
-                        <a-button type="primary" style="background-color: #0b8235 !important;border-color: #0b8235 !important;" icon="edit" size="large">
-                            Editar
-                        </a-button>
-                        <a-button type="primary" style="margin-left:10px;background-color: #ff4d4f !important;border-color: #ff4d4f !important;" icon="delete" size="large">
-                            Eliminar
-                        </a-button>
-                    </a-col>
-                </a-row>
+            <a-col v-else :span="24">
+                <a-col :span="6" :style="{padding:'10px'}">
+                    <a-card title="Grado" :bordered="false" style="width: 100%;">
+                        <a-switch default-checked @change="onChange" />
+                    </a-card>
+                    <a-card title="Rango" :bordered="false" style="width: 100%;">
+                        <a-switch default-checked @change="onChange" />
+                    </a-card>
+                </a-col>
+                <a-col :span="18">
+                    <a-row>
+                        <a-col :span="8">
+                            <b-form-group class="col-md-11" label="Escalafon" label-for="Escalafon" label-cols-sm="12" label-align-sm="right" >
+                                <b-form-input id="Escalafon"></b-form-input>
+                            </b-form-group>
+                        </a-col>
+                        <a-col :span="8">
+                            <b-form-group class="col-md-11" label="Nombre" label-for="Nombre" label-cols-sm="12" label-align-sm="right" >
+                                <b-form-input id="Nombre"></b-form-input>
+                            </b-form-group>
+                        </a-col>
+                        <a-col :span="8">
+                            <b-form-group class="col-md-11" label="Apellido Materno" label-for="Apellido Materno" label-cols-sm="12" label-align-sm="right" >
+                                <b-form-input id="Materno"></b-form-input>
+                            </b-form-group>
+                        </a-col>
+                        <a-col :span="8">
+                            <b-form-group class="col-md-11" label="Apellido Paterno" label-for="Apellido Paterno" label-cols-sm="12" label-align-sm="right" >
+                                <b-form-input id="Paterno"></b-form-input>
+                            </b-form-group>
+                        </a-col>
+                        <a-col :span="8">
+                            <b-form-group class="col-md-11" label="Telefono" label-for="Telefono" label-cols-sm="12" label-align-sm="right" >
+                                <b-form-input id="Telefono"></b-form-input>
+                            </b-form-group>
+                        </a-col>
+                    </a-row>
+                    <a-row>
+                        <a-col :span="24" :style="{marginTop:'20px'}">
+                            <a-button type="primary" style="background-color: #0b8235 !important;border-color: #0b8235 !important;" icon="edit" size="large">
+                                Editar
+                            </a-button>
+                            <a-button type="primary" style="margin-left:10px;background-color: #ff4d4f !important;border-color: #ff4d4f !important;" icon="delete" size="large">
+                                Eliminar
+                            </a-button>
+                        </a-col>
+                    </a-row>
+                </a-col>
             </a-col>
         </a-row>
     </div>
@@ -97,11 +99,15 @@ export default {
     name: "VistaUsuario",
     data() {
         return {
-            Usuario:{}
+            Usuario:{},
+            Editando:false
         }
     },
     components:{FotoCircular,RangosUser},
     methods:{
+        EditarStatusSet(check){
+            this.Editando=check;
+        },
         onChange(checked) {
             console.log(`a-switch to ${checked}`);
         },
